713c5715311a773e2fff0113330976e3da4b5569
NimeshPiyumantha <nimeshpiyumantha12@gmail.com>
2023-03-16 02:42:50 +0530

    Update Content

--- public/script.js
+++ public/script.js
@@ -2,3 +2,11 @@ var navbar = document.getElementById("navbar");
 const toggle_nav = ()=>{
     navbar.classList.toggle("hidden");
 }
+
+//Close menu is window size increases
+window.onresize=()=>{
+    let viewportwidth=window.innerWidth;
+    if(viewportwidth>1050){
+        navbar.classList.add("hidden");
+    }
+}
