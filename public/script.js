20d19ef7711d426d7fa0f7087d5e49d829d950f3
NimeshPiyumantha <nimeshpiyumantha12@gmail.com>
2023-03-17 17:36:48 +0530

    Update Content

--- public/script.js
+++
@@ -1,8 +0,0 @@
-const btn = document.getElementById('menu-btn');
-const nav = document.getElementById('menu');
-
-btn.addEventListener('click', () => {
-  btn.classList.toggle('open');
-  nav.classList.toggle('flex');
-  nav.classList.toggle('hidden');
-});
